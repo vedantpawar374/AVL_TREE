ba7dd53d9e06451506f6326718d6abebc89bbd33
Vedant Pawar <vedantpawar00001.in>
2026-01-18 15:38:10 +0530

    code for deleting node from avl was added

--- AVL_TREE/AVL_TREE.cpp
+++ AVL_TREE/AVL_TREE.cpp
@@ -164,12 +164,116 @@ public:
         return root;
 
     }
+    //deleting a node from avl tree;
+    void deleteNode()
+    {
+        int x;
+        cout << "\nENter node to be deleted";
+        cin >> x;
+        root=deleteRec(root, x);
+    }
+    Node* deleteRec(Node* root, int x)
+    {
+        if (root == NULL)//data is not present
+        {
+            cout << "\ndata is not present ";
+            return NULL;
+        }
+        if (root->data > x)
+        {
+            root->left = deleteRec(root->left, x);//x is less than root->data so x if present it should be at the left of the root
+            //as this is calling the deleteRec fun it may happen while delting the down tree the balance factor of the upper tree may fall
+            //i am deleting the node from the left side of the tree so balance falctor if misplaced it might be -2 
+            if (balFactor(root) == -2)
+            {
+                   if(balFactor(root->right)==0 || balFactor(root->right)==-1)//
+                   {
+                       root = rotateLeft(root);
+                   }
+                   else//balfactor(t)==1
+                   {
+                       root = rotateRightLeft(root);
+                   }
+            }
+            return root;
+        }
+        else if (root->data < x)//if x is greater then it is present at the right
+        {
+            root->right = deleteRec(root->right, x);
+            if (balFactor(root) == 2)
+            {
+                if (balFactor(root->left) == 0 || balFactor(root->left) == 1)
+                {
+                    root = rotateRight(root);
+                }
+                else
+                {
+                    root = rotateLeftRight(root);
+                }
+            }
+            return root;
+        }
+        //as data is not greater nor smaller then data is found;
+        if (root->left == NULL && root->right == NULL)//leaf node 
+        {
+            delete root;
+            return NULL;
+        }
+        //check for node with one child
+        else if (root->left == NULL)//data is found at root;
+        {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        //if these cases failed node to be deleted have both the childs
+        else
+        {
+            Node* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right=deleteRec(root->right, temp->data);
+            //after deleting from the right the balfactor might be unbalanced (deleted from right bal factor becomes 2)
+            if (balFactor(root) == 2)
+            {
+                if (balFactor(root->right) == 1 || balFactor(root) == 0)
+                {
+                    root = rotateRight(root);
+                }
+                else// bal =-1
+                {
+                    root = rotateLeftRight(root);
+                }
+            }
+            return root;
+        }
+    }
+    Node* findMin(Node* root)
+    {
+        Node* temp = root;
+        while (temp->left != NULL)
+        {
+            temp = temp->left;
+        }
+        return temp;
+    }
 };
 int main()
 {
     std::cout << "Hello World!\n";
     AVLTree tree;
     tree.insert();
+    tree.showRec();
+    tree.deleteNode();
+    tree.showRec();
+    tree.deleteNode();
+    tree.showRec();
+    tree.deleteNode();
     tree.showRec();
 
 }
